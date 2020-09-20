219072fc8eb0c354cc133d9c5050eb103f064286
chingliu10 <56655368+chingliu10@users.noreply.github.com>
2020-09-20 09:40:06 +0300

    updated cart price on server after updating cart

--- removeItem.js
+++ removeItem.js
@@ -1,19 +1,19 @@
 module.exports = function removeItem(request, response, itemRemoved)   {
-    console.log(request.session.cart.products)
-    console.log("top are items in cart")
-    console.log(request.session.cart.products[2])
+    
     let arr = request.session.cart.products;
+    let price;
+    console.log("array is")
     console.log(arr)
     for(let x = 0; x < arr.length; x++){
       
         if ( arr[x].name == itemRemoved.trim()){
-            arr.splice([1], 1)
+            price = arr[x].price
+            arr.splice(x, 1)  
             break;
         }
       
     }
-    console.log("updated products are")
-    console.log(arr)
-
-    response.json({ display  : " item removed"} )
+    
+    request.session.cart.total = request.session.cart.total - price 
+    response.json({ total  : price } )
 }
